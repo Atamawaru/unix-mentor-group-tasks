92b74ac5ca8d9fbfb04b772ae4c681e83562880e
Atamawaru <karolis.markevicius4@gmail.com>
2026-04-14 20:37:48 +0000

    Add function to append dates to txt files in given directory

--- bash-scripts-1/renamer.sh
+++ bash-scripts-1/renamer.sh
@@ -4,17 +4,19 @@ set -eou pipefail
 
 argument=""
 if [ "$1" != "" ]; then
-	argument=$1
-	if ! [ -d "$argument" ]; then
-		echo "Error. Not a directory." >&2
-		exit 1
-	fi
+    argument=$1
+    if ! [ -d "$argument" ]; then
+        echo "Error. Not a directory." >&2
+        exit 1
+    fi
 fi
 argument="${argument%/}"
-echo "$argument"
+date_to_append=$(date +%Y%m%d)
 for file in "$argument"/*; do
-	file="${file##*/}"
-	if [[ -f "$file" && "$file" = *.txt ]]; then
-		echo "$file this shit is a txt file"
-	fi
+    old_file_name="${file##*/}"
+    if [[ -f "$old_file_name" && "$old_file_name" = *.txt ]]; then
+        new_file_name="${date_to_append}_${old_file_name}"
+        new_file_name="${argument%/}/${new_file_name}"
+        mv "$file" "$new_file_name"
+    fi
 done
