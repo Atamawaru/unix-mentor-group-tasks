5263adcdcd54dab4376921a7c438f65632843cdb
Atamawaru <karolis.markevicius4@gmail.com>
2026-04-14 21:17:57 +0000

    Skip files that have date prefixed already

--- bash-scripts-1/renamer.sh
+++ bash-scripts-1/renamer.sh
@@ -4,19 +4,21 @@ set -eou pipefail
 
 argument=""
 if [ "$1" != "" ]; then
-    argument=$1
-    if ! [ -d "$argument" ]; then
-        echo "Error. Not a directory." >&2
-        exit 1
-    fi
+	argument=$1
+	if ! [ -d "$argument" ]; then
+		echo "Error. Not a directory." >&2
+		exit 1
+	fi
 fi
 argument="${argument%/}"
 date_to_append=$(date +%Y%m%d)
 for file in "$argument"/*; do
-    old_file_name="${file##*/}"
-    if [[ -f "$file" && "$old_file_name" = *.txt ]]; then
-        new_file_name="${date_to_append}_${old_file_name}"
-        new_file_name="${argument%/}/${new_file_name}"
-        mv "$file" "$new_file_name"
-    fi
+	old_file_name="${file##*/}"
+	if [[ -f "$file" && "$old_file_name" = *.txt ]]; then
+		if ! [[ "$old_file_name" =~ [0-9]{8} ]]; then
+			new_file_name="${date_to_append}_${old_file_name}"
+			new_file_name="${argument%/}/${new_file_name}"
+			mv "$file" "$new_file_name"
+		fi
+	fi
 done
