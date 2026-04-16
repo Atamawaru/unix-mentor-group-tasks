562d83a8ae955d6bb91212aefa1e8aa83ff4e8a4
Atamawaru <karolis.markevicius4@gmail.com>
2026-04-16 17:10:35 +0000

    Fix script paramenters  not working in edge cases

--- bash-scripts-1/copy.sh
+++ bash-scripts-1/copy.sh
@@ -1,27 +1,52 @@
-#!/bin/bash
+#!/usr/bin/bash
 
+is_verbose=0
+pattern="*.log"
+source_dir=""
+target_dir=""
 usage() {
-    cat <<'EOF'
--p / --pattern <glob>	file pattern to match (default: *.log)
--v / --verbose	       	print each filename as it is copied
--h / --help             show usage and exit
+	cat <<'EOF'
+Usage: ./copy.sh (OPTIONS) <source_dir> <target_dir>
+
+Flags:
+        -p / --pattern <glob>	file pattern to match (default: *.log)
+        -v / --verbose	       	print each filename as it is copied
+        -h / --help             show usage and exit
 EOF
 }
 
 if [[ $# -eq 0 ]]; then
-    usage
-    exit 1
+	usage
+	exit 1
 fi
 
 while [[ $# -gt 0 ]]; do
-    case $1 in
-    -h | --help)
-        usage
-        exit 0
-        ;;
-    ?)
-        echo "Error. Unknown command."
-        exit 1
-        ;;
-    esac
+	case $1 in
+	-p | --pattern)
+		if [[ -z $2 || "${2:0:1}" == "-" ]]; then
+			echo "Error. -p/--patern requires an argument."
+			exit 1
+		fi
+		pattern=$2
+		shift 2
+		;;
+	-v | --verbose)
+		is_verbose=1
+		shift 1
+		;;
+	-h | --help)
+		usage
+		exit 0
+		;;
+	*)
+		if [[ -z $source_dir && "${1:0:1}" != "-" && -z $target_dir && "${2:0:1}" != "-" ]]; then
+			source_dir=$1
+			target_dir=$2
+			shift 2
+		else
+			echo "Error. Unknown $1 command."
+			exit 1
+		fi
+		;;
+	esac
 done
