21ee2a58f6141b5c9bfe2576ca94b2e8c646927f
Atamawaru <karolis.markevicius4@gmail.com>
2026-04-17 09:44:57 +0000

    Add argument parsing to check.sh, cycle through inputted processes

---
+++ bash-scripts-1/check.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/bash
+
+usage() {
+	cat <<'EOF'
+Usage: ./check.sh (OPTIONS) <processes>
+
+Flags:
+        -q / --quiet          suppress per-process output, only print final report.
+        -h / --help             show usage and exit
+EOF
+}
+non_flag_args=()
+non_flag_count=0
+is_quiet=0
+if [[ $# -eq 0 ]]; then
+	usage
+	exit 1
+fi
+while [[ "$#" -gt 0 ]]; do
+	case "$1" in
+	-q | --quiet)
+		is_quiet=1
+		shift 1
+		;;
+	-h | --help)
+		usage
+		exit 0
+		;;
+	-* | --*)
+		echo "Error. Unknown $1 command."
+		exit 1
+		;;
+	*)
+		if [[ "${1:0:1}" != "*" ]]; then
+			non_flag_args[non_flag_count]=$1
+			((non_flag_count++))
+			shift 1
+		fi
+		;;
+	esac
+done
+echo "Got processes: ${non_flag_args[*]}"
+for process in "${non_flag_args[@]}"; do
+	output=""
+	if [[ $(pgrep "$process") != "" ]]; then
+		echo "yes"
+	fi
+done
