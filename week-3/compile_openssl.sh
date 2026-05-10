87e218b02f19af9fb6e909e972241e4844f61d18
Atamawaru <karolis.markevicius4@gmail.com>
2026-05-10 17:28:03 +0000

    Properly include lib paths for compilation

--- week-3/compile_openssl.sh
+++ week-3/compile_openssl.sh
@@ -7,7 +7,7 @@ fi
 user_home_dir=$(eval echo "~$SUDO_USER")
 openssl_dir=$user_home_dir/opt/openssl
 openssl_source_repo="https://github.com/openssl/openssl.git"
-openssl_source_branch="OpenSSL_1_0_2-stable"
+openssl_source_branch="OpenSSL_1_1_1-stable"
 
 if ! [[ -d $openssl_dir ]]; then
     echo "$openssl_dir does not exist. Creating it."
@@ -51,11 +51,12 @@ fi
 
 echo "Installing OpenSSL source code..."
 git clone -b $openssl_source_branch --single-branch --depth=1 "$openssl_source_repo"
-
 echo "Compiling OpenSSL source code"
-cd ./openssl || return 1; ./config --prefix="$openssl_dir" --openssldir="$openssl_dir"
+cd ./openssl || return 1; ./config --prefix="$openssl_dir" --openssldir="$openssl_dir" -Wl,-rpath,"$openssl_dir/lib" 
 cd ..
 make -C ./openssl -j"$(nproc)"
 make -C ./openssl test
 make -C ./openssl install
+echo "Openssl installed in $openssl_dir"
+"$openssl_dir"/bin/openssl version
 rm -rf openssl
